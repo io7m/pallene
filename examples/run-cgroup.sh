51fbbe385ee0e851a8060db7cee5d553d953ffc5
Mark Raynsford <code@io7m.com>
2017-11-05 11:01:45 +0000

    Update examples

--- examples/run-cgroup.sh
+++ examples/run-cgroup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
 cgcreate -g memory:/pallene
-echo $((32 * 1024 * 1024)) > /sys/fs/cgroup/memory/pallene/memory.limit_in_bytes
-echo $((32 * 1024 * 1024)) > /sys/fs/cgroup/memory/pallene/memory.memsw.limit_in_bytes
+echo $((64 * 1024 * 1024)) > /sys/fs/cgroup/memory/pallene/memory.memsw.limit_in_bytes
+echo $((64 * 1024 * 1024)) > /sys/fs/cgroup/memory/pallene/memory.limit_in_bytes
 
+exec cgexec -g memory:/pallene "$@"
